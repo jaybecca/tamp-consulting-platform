4f1ae630c9f62cc9de9d78e2894de8483002ea13
Rebecca E. Ogbeide <beccaogbeide@gmail.com>
2026-07-23 22:03:56 +0100

    complete TAMP Consulting landing page with contact improvements, floating buttons, and footer

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.pageYOffset > 300){
+        backToTop.classList.add("show");
+    }else{
+        backToTop.classList.remove("show");
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
+
